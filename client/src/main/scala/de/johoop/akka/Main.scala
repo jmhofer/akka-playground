aed54833d0cb67beb99ccb49b9dc34cfc505b24d
Joachim Hofer <jmhofer.github@johoop.de>
2012-08-12 14:56:52 +0200

    trying out possible authentication schemes

--- client/src/main/scala/de/johoop/akka/Main.scala
+++ client/src/main/scala/de/johoop/akka/Main.scala
@@ -11,16 +11,16 @@ object Main extends App {
   implicit val timeout = Timeout(5 seconds)
 
   val system = ActorSystem("Client")
-  val server = system.actorFor("akka://Server@localhost:2554/user/server")
+  val auth = system.actorFor("akka://Server@localhost:2554/user/secure")
 
   try {
-    val response = server ? Hello
-    val result = Await.result(response, timeout.duration)
-    println(result)
+    val response = auth ? "auth"
+    val result = Await.result(response, timeout.duration).asInstanceOf[ActorRef]
+    result ! Hello
 
+    println(result)
+    
   } finally {
     system.shutdown
   }
 }
-
-
--- server/src/main/scala/de/johoop/akka/Server.scala
+++ server/src/main/scala/de/johoop/akka/Server.scala
@@ -3,17 +3,39 @@ package de.johoop.akka
 import akka.actor._
 import akka.event.Logging
 import akka.kernel.Bootable
+import com.typesafe.config.ConfigFactory
+import scala.util.Random
 
 class Server extends Bootable {
   val system = ActorSystem("Server")
 
-  def startup = system.actorOf(Props[ServerActor], "server")
+  def startup = {
+    system.actorOf(Props[AuthActor], "secure")
+  }
   def shutdown = system.shutdown
 }
 
-class ServerActor extends Actor {
+class AuthActor extends Actor {
   val log = Logging(context.system, this)
+  val random = new Random
+  
+  def receive = {
+    case "auth" => {
+      log.info("auth") // normally, authentication would happen here...
+      sender ! context.actorOf(Props[HelloActor], randomActorName)
+    }
+  }
+  
+  def randomActorName: String = {
+    // akka says: must conform to [-\w:@&=+,.!~*'_;][-\w:@&=+,.!~*'$_;]*
+    val validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789:@&=+,.!~*'_;"
+    Array.fill(256) { validChars.charAt(random.nextInt(validChars.size)) } mkString
+  }
+}
 
+class HelloActor extends Actor {
+  val log = Logging(context.system, this)
+  
   def receive = {
     case Hello => {
       log.info("Someone said hello to me! Yay!")
